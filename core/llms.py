26a63d128c7dce17172e55c093d6a036c593312c
PhucHT <phucht.it2023@gmail.com>
2024-07-28 14:27:00 +0700

    update model list

--- core/llms.py
+++ core/llms.py
@@ -6,4 +6,8 @@ genai.configure(api_key=GOOGLE_API_KEY)
 def get_model(model_name):
     return genai.GenerativeModel(model_name)
 
-list_model_name = [model.name for model in genai.list_models() if "generateContent" in model.supported_generation_methods]
+list_model_name = [
+    model.name 
+    for model in genai.list_models() 
+    if "generateContent" in model.supported_generation_methods and "latest" in model.name and "vision" not in model.name
+]
